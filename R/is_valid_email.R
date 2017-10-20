77f1b67093c32b9c8723d47dd4a8810754455126
mjfii <mjfii@live.com>
2017-10-20 10:24:49 -0600

    initial code commit

---
+++ R/is_valid_email.R
@@ -0,0 +1,11 @@
+is_valid_email <- function(x) {
+
+  if (!is.character(x)) {
+    cat('Vector argument must be a character.\n')
+    return(NA)
+  }
+
+  proper_form <- "^(?=(.{1,64}@.{1,255}))([!#$%&'*+\\-\\/=?\\^_`{|}~a-zA-Z0-9}]{1,64}(\\.[!#$%&'*+\\-\\/=?\\^_`{|}~a-zA-Z0-9]{0,}){0,})@((\\[(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}\\])|([a-zA-Z0-9-]{1,63}(\\.[a-zA-Z0-9-]{2,63}){1,}))$"
+
+  return(grepl(proper_form, x, perl=TRUE))
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 
+
+```R
+em <- c('mick.@gmail.com','mick.flanigan@gmail.co.m')
+
+is_valid_email(em)
+```
